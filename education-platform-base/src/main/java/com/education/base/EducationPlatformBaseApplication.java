84b918bbff8a2ceebce61f1958a03d8bee253d8e
kkoneone11 <604065124@qq.com>
2023-08-03 22:55:30 +0800

    初始化父类工程和基础工程

---
+++ education-platform-base/src/main/java/com/education/base/EducationPlatformBaseApplication.java
@@ -0,0 +1,13 @@
+package com.education.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationPlatformBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationPlatformBaseApplication.class, args);
+    }
+
+}
