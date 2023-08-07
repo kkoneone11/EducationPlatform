ba65848fec3b715b818c0a07c7a098814e8e0885
kkoneone11 <604065124@qq.com>
2023-08-07 20:14:32 +0800

    初始化system工程

---
+++ education-platform-system/education-platform-system-service/src/main/java/com/education/system/mapper/DictionaryMapper.java
@@ -0,0 +1,15 @@
+package com.education.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.education.system.model.po.Dictionary;
+
+/**
+ * <p>
+ * 数据字典 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface DictionaryMapper extends BaseMapper<Dictionary> {
+
+}
