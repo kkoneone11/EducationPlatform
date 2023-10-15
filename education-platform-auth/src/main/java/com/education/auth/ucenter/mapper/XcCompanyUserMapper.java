ed3fe40f6af02c8867a063469ed1f646cebb7470
kkoneone11 <604065124@qq.com>
2023-10-15 11:08:37 +0800

    OAuth基本配置

---
+++ education-platform-auth/src/main/java/com/education/auth/ucenter/mapper/XcCompanyUserMapper.java
@@ -0,0 +1,18 @@
+package com.education.auth.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import com.education.auth.ucenter.model.po.XcCompanyUser;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+@Mapper
+public interface XcCompanyUserMapper extends BaseMapper<XcCompanyUser> {
+
+}
---
+++ education-platform-auth/src/main/java/com/education/auth/ucenter/mapper/XcPermissionMapper.java
@@ -0,0 +1,17 @@
+package com.education.auth.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.education.auth.ucenter.model.po.XcPermission;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+@Mapper
+public interface XcPermissionMapper extends BaseMapper<XcPermission> {
+
+}
